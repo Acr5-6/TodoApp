eb2c295077d65aff96f67666037900850aac9f62
Acr5-6 <45597621+Acr5-6@users.noreply.github.com>
2019-06-01 23:59:58 +0200

    step2

---
+++ src/main/java/com/polytech/TodoApp/api/TaskController.java
@@ -0,0 +1,41 @@
+package com.polytech.TodoApp.api;
+
+import com.polytech.TodoApp.business.Task;
+import com.polytech.TodoApp.business.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+
+    @GetMapping("/getTasks")
+    public List<Task> getTasks(){
+        System.out.println("getTasks");
+        return taskService.get_Tasks();
+    }
+
+    @PostMapping("/addTask")
+    public void addTask(@RequestBody String content){
+        System.out.println("addTask");
+        taskService.add_task(new Task(content));
+    }
+
+    @DeleteMapping("/deleteTask/{idtask}")
+    public void deletetask(@PathVariable("idtask") int id_task){
+        System.out.println("deleteTask");
+        taskService.delete_task(id_task);
+    }
+
+    @PutMapping("/updateTask/{idtask}/{newContent}")
+    public void update(@PathVariable("idtask") int id_task, @PathVariable("newContent") String newcontent) {
+        System.out.println("updateTask");
+        taskService.update_task(id_task, newcontent);
+    }
+
+}
